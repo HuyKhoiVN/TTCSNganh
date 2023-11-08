0f1bbde73d00d65d23575747e4cdd2fe1ed1b89a
“nesey” <vutungtam8@gmail.com>
2023-11-08 17:25:22 +0700

    TT add màn hình

--- CustomerManager/InteractionManage.cs
+++ CustomerManager/InteractionManage.cs
@@ -16,5 +16,40 @@ namespace CustomerManager
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
